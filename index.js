00f1a0ce175cb76ddbed8918b85ac1b12e739551
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-09-26 09:24:33 +0200

    Preliminary pages.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var sys = require('sys'),
     fs = require('fs'),
@@ -7,27 +8,55 @@ var sys = require('sys'),
 
 var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+// configure objects
+
+var reddit = new rawjs("raw.js example script");
+
+var appUrl = "http://localhost:" + config.port;
+var callbackUrl = appUrl + "/auth";
+reddit.setupOAuth2(config.appId, config.appSecret, callbackUrl);
+var loginUrl = reddit.authUrl("some_random_state", ['identity','edit','history']);
+
+// pages
+
+var page_welcome = function(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Please log in to reddit</h1><p><a href="' + loginUrl + '">Login</a>');
+};
+
+var page_404 = function(req,res) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not found\n');
+};
+
+
 // start server
 
 http.createServer(function (req, res) {
     var url_parts = url.parse(req.url);
     sys.puts(url_parts.pathname);
+    
+    switch (url_parts.pathname) {
+    case "/":
+        page_welcome(req,res);
+        break;
 
-    setTimeout(function () {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello World\n');
+    case "/auth":
+        page_oauth(req,res);
+        break;
 
-    }, 2000);
+    case "/delete":
+        page_delete(res,req);
+        break;
+
+    default:
+        page_404(res, req);
+        break;
+    }
 }).listen(config.port);
 
-var reddit = new rawjs("raw.js example script");
-
-var callbackUrl = "http://localhost:" + config.port + "/";
-reddit.setupOAuth2(config.appId, config.appSecret, callbackUrl);
 
 
-console.log("Running local server on: " + callbackUrl);
+console.log("Running local server on: " + appUrl);
 
-// We're ready. Ask user to authenticate!
-var loginUrl = reddit.authUrl("some_random_state", ['identity','edit','history']);
-console.log ("Login URL: " + loginUrl);
+
