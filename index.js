aa09d44703794695db95d2a4f445c97dcaa62656
Jostein Kjønigsen <jostein@kjonigsen.net>
2015-10-04 21:53:46 +0200

    Clean-up and restructuring.
    - Remove multitenancy for now.
    - Make configuration only part of the app with an UI.

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var app = express();
 app.use(cookieParser());
 var Snoocore = require('snoocore');
 
-var uniqId = 0; // unique id for our user
-var accounts = {}; // Snoocore instances. Each instance is a different account
-
 // configuration
 
 var load_config = function () {
@@ -30,97 +27,96 @@ var appUrl = "http://localhost:" + config.port;
 var authPage = "/auth";
 var callbackUrl = appUrl + authPage;
 
-function getInstance(accountId) {
-
-    // Check if we already have an instance with this id. If
-    // so, use this instance
-    if (accounts[accountId]) {
-        return accounts[accountId];
+var reddit = new Snoocore({
+    userAgent: 'reddit comment cleaner',
+    oauth: {
+        type: 'explicit',
+        duration: 'permanent',
+        key: config.appId,
+        secret: config.appSecret,
+        redirectUri: callbackUrl,
+        scope: [ 'identity', 'edit', 'history' ]
     }
+});
 
-    // Else, return a new instance
-    return new Snoocore({
-        userAgent: 'reddit comment cleaner',
-        oauth: {
-            type: 'explicit',
-            duration: 'permanent',
-            key: config.appId,
-            secret: config.appSecret,
-            redirectUri: callbackUrl,
-            scope: [ 'identity', 'edit', 'history' ]
-        }
-    });
-}
-
-
-// pages
+// configuration pages, for first time running of application.
 
 app.get('/', function (req, res) {
-    var accountId = req.cookies ? req.cookies.account_id : void 0;
-
-    // We have an account, redirect to the authenticated route
-    if (accountId && typeof accounts[accountId] === 'function') {
-        return res.redirect('/posts');
-    }
-
-    var reddit = getInstance();
     return res.send('<h1>Please log in to reddit</h1><p><a href="' + reddit.getAuthUrl() + '">Login</a>');
 });
 
 // does not account for hitting "deny" / etc. Assumes that
 // the user has pressed "allow"
 app.get(authPage, function(req, res) {
-    var accountId = ++uniqId; // an account id for this instance
-    var instance = getInstance(); // an account instance
-
-    console.log(req.query.code);
-
     // In a real app, you would save the refresh token in
     // a database / etc for use later so the user does not have
     // to allow your app every time...
-    return instance.auth(req.query.code).then(function(refreshToken) {
+    return reddit.auth(req.query.code).then(function(refreshToken) {
         // Store the account (Snoocore instance) into the accounts hash
-        accounts[accountId] = instance;
-
         config.refresh_token = refreshToken;
         save_config(config);
 
-        // Set the account_id cookie in the users browser so that
-        // later calls we can refer to the stored instance in the
-        // account hash
-
-        console.log(accounts);
-        res.cookie('account_id', String(accountId), { maxAge: 900000, httpOnly: true });
-
         // redirect to the authenticated route
-        return res.redirect('/posts');
+        return res.redirect('/done');
     });
 });
 
-app.get('/posts', function(req, res) {
+app.get('/done', function(req, res) {
+    runApp();
+    res.send("Application configured and running. Restart to disable configuration interface.");
+});
 
-    var accountId = req.cookies ? req.cookies.account_id : void 0;
+// actual application-logic
 
-    // If the user has not authenticated bump them back to the main route
-    if (!accountId || typeof accounts[accountId] === 'undefined') {
-        return res.redirect('/');
-    }
+function getUserName() {
+    return reddit('/api/v1/me').get().then(function(result) {
+        var username = result.name;
+        console.log("Logged in as: " + username);
+        return username;
+    });
+};
 
+function getLastCommentToKeep(user, numToKeep) {
+    return reddit('/user/' + user + '/comments/').listing({
+        limit: numToKeep
+    }).then(function (slice) {
+        if (slice.empty) {
+            console.log("No comments returned when trying to get last to keep!");
+            return null;
+        } else {
+            var lastComment = slice.allChildren[slice.count - 1];
+            var lastId = lastComment.id;
+            console.log("Last comment to keep: " + lastId);
+            return lastId;
+        }
+    });
+}
+
+function deleteComments(slice, callback, error) {
+    // TODO:
+    // 1. delete single comment
+    // 2. recurse into self with remaining if any
+    // 3. callback when done, or call error.
+}
+
+
+function runApp() {
     // Print out stats about the user, that's it.
-    return accounts[accountId]('/api/v1/me').get().then(function(result) {
-        var me = JSON.stringify(result, null, 4);
-
-        return accounts[accountId]('/user/' + result.name + '/comments/').get().then(function (result2) {
-            var comments = JSON.stringify(result2, null, 4);
-            return res.send("ME: " + me + " COMMENTS:" + comments);
-        });
-
-        // TODO: display POSTS!
+    getUserName().then(function (me) {
+        return getLastCommentToKeep(me, 100);
+    }).then(function (lastId) {
+        console.log("TODO: actually delete comments.");
     });
-});
+};
 
-// start server
+// start configuration app-server
 
-var server = app.listen(config.port, function () {
-  console.log('Example app listening at ' + appUrl);
-});
+if (config.refresh_token == 'undefined') {
+    var server = app.listen(config.port, function () {
+        console.log('Example app listening at ' + appUrl);
+    });
+} else {
+    reddit.refresh(config.refresh_token).then(function() {
+        runApp();
+    });
+}
